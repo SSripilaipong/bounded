d3a8d512f1d8ab9bdd463294f7786541d753611f
SSripilaipong <SHSnail@mail.com>
2022-05-24 17:19:18 +0700

    feat(usecase_signature): test_should_raise_AnnotationError_when_define_method_another_method_with_parameter_annotation_that_is_not_a_type
    Refs: #3

--- tests/test_usecase/test_parameter_type.py
+++ tests/test_usecase/test_parameter_type.py
@@ -38,3 +38,13 @@ def test_should_raise_AnnotationError_when_define_method_my_method_with_paramete
                 pass
 
     assert str(ex.value) == "Parameters of usecase's public method 'my_method()' must be annotated with types"
+
+
+def test_should_raise_AnnotationError_when_define_method_another_method_with_parameter_annotation_that_is_not_a_type():
+    with raises(AnnotationError) as ex:
+
+        class MyUsecase(Usecase):
+            def another_method(self, a: 1234) -> int:
+                pass
+
+    assert str(ex.value) == "Parameters of usecase's public method 'another_method()' must be annotated with types"
